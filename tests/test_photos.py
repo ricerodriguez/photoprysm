454bab2d67e657564e545b2c58a604a7a3792538
Rice Rodriguez <victoriarice@protonmail.com>
2025-02-02 15:07:14 -0500

    Add more mocks. Start test_photos. Remove obsolete loader

--- tests/test_photos.py
+++ tests/test_photos.py
@@ -7,38 +7,46 @@ from dataclasses import asdict
 
 from photoprysm import core
 from photoprysm import photos
-import photoprysm
+
 from .test_core import *
-from .mock_responses.loader import get_mock_response
 
 @responses.activate
-def test_get_photos_count1_quality0(mock, server_api, session):
-    responses.get(
-        url = urljoin(server_api, 'photos?count=1&quality=0'),
-        status = 200,
-        json = mock
-    )
-    photos_list = photos.get(session, server_api, count = 1)
-    assert isinstance(photos_list, list)
-
-@responses.activate
-def test_get_photo_by_uid(mock, server_api, session):
-    uid = mock['UID']
+def test_get_by_uid(mock_photo, server_api, session):
+    uid = mock_photo['json']['UID']
     responses.get(
         url = urljoin(server_api, f'photos/{uid}'),
-        status = 200,
-        json = mock
-    )
+        **mock_photo)
     photo = photos.get_by_uid(session, server_api, uid)
     assert photo.uid == uid
+    
+# @responses.activate
+# def test_get_photos_count1_quality0(mock, server_api, session):
+#     responses.get(
+#         url = urljoin(server_api, 'photos?count=1&quality=0'),
+#         status = 200,
+#         json = mock
+#     )
+#     photos_list = photos.get(session, server_api, count = 1)
+#     assert isinstance(photos_list, list)
 
-@responses.activate
-def test_approve_photo(mock, server_api, session):
-    photo_uid = mock['photo']['UID']
-    responses.post(
-        url = urljoin(server_api, f'photos/{photo_uid}/approve'),
-        status = 200,
-        json = mock
-    )
-    photo = photoprysm.Photo(uid = photo_uid)
-    photos.approve(session, server_api, photo)
+# @responses.activate
+# def test_get_photo_by_uid(mock, server_api, session):
+#     uid = mock['UID']
+#     responses.get(
+#         url = urljoin(server_api, f'photos/{uid}'),
+#         status = 200,
+#         json = mock
+#     )
+#     photo = photos.get_by_uid(session, server_api, uid)
+#     assert photo.uid == uid
+
+# @responses.activate
+# def test_approve_photo(mock, server_api, session):
+#     photo_uid = mock['photo']['UID']
+#     responses.post(
+#         url = urljoin(server_api, f'photos/{photo_uid}/approve'),
+#         status = 200,
+#         json = mock
+#     )
+#     photo = photoprysm.Photo(uid = photo_uid)
+#     photos.approve(session, server_api, photo)
