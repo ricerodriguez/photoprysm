d436aaa62d6121aa9886e2480e0d25575e57074c
Rice Rodriguez <victoriarice@protonmail.com>
2025-02-09 15:02:46 -0500

    Update version

--- src/photoprysm/__init__.py
+++ src/photoprysm/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for Photoprysm."""
 __author__ = """Rice Rodriguez"""
 __email__ = 'victoriarice@protonmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 # Make the public members of the core accessible from the top
 from .core import Client
